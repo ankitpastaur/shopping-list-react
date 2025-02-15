3af51b14d0e8e296ad8348b816627e954823b2bf
ankitpastor <pastorankit18@gmail.com>
2025-02-14 21:58:48 -0500

    Added debouncing for api call and updated logic

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import "./App.css";
 function App() {
   const [product, setProduct] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [bucketList, setBucketList] = useState([]);
+
+  useEffect(() => {
+    if (product.length >= 2) {
+      const timer = setTimeout(() => {
+        fetchItems(product);
+      }, 500); // Debouncing API call
+
+      return () => clearTimeout(timer); // Cleanup previous timer
+    } else {
+      setSuggestions([]); // Clear suggestions if input is empty or less than 2 chars
+    }
+  }, [product]); // Runs whenever `product` changes
 
   const handleInput = (e) => {
     console.log(e.target.value);
@@ -22,28 +35,34 @@ function App() {
     setSuggestions(filteredProducts);
   };
 
-  useEffect(() => {
-    if (product.length >= 2) {
-      //make an api call
-      fetchItems(product);
-    } else {
-      setSuggestions([]);
+  const handleShoppingList = (e) => {
+    const idx = e.target.getAttribute("data-id");
+    if (idx) {
+      const obj = {
+        id: Date.now(),
+        data: suggestions[idx],
+        isDone: false,
+      };
+      const copyBucketList = [...bucketList];
+      copyBucketList.push(obj);
+      setBucketList(copyBucketList);
     }
-  }, [product]);
+  };
+
   return (
     <div className="App">
       <h1>My Shopping List</h1>
       <div>
         <input className="input-item" value={product} onChange={handleInput} />
+        <div className="shopping-list" onClick={handleShoppingList}>
+          {suggestions.map((item, index) => (
+            <div key={item.id} data-id={index} className="suggestion-item">
+              {item.title}
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="shopping-list"></div>
-      <div className="shopping-list">
-        {suggestions.map((item) => (
-          <div key={item.id} className="suggestion-item">
-            {item.title}
-          </div>
-        ))}
-      </div>
+      <div className="bucketList"></div>
     </div>
   );
 }
